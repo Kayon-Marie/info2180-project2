bbce129ea6a6e45c3007e099092226c953cb0c6f
Kayon-Marie <k.marie.douglas@hotmail.com>
2018-11-05 18:42:49 -0500

    Fixed position of titles. Movement of titles to whitespace

--- fifteen.js
+++ fifteen.js
@@ -1,8 +1,12 @@
 window.onload = function(){
     const boxes = document.querySelectorAll('#puzzlearea div');
+    let blankTile = ['300px', '300px'];
+    const shuffleButton = document.getElementById('shufflebutton');
 
     setUp();
     display();
+    selectPieces();
+    shuffleButton.addEventListener('click',shuffle);
     function setUp(){
         boxes.forEach(d =>{
             d.classList.add("puzzlepiece");
@@ -11,13 +15,35 @@ window.onload = function(){
 
     function display(){
         num = 0;
-        for(var p = 0; p < 16; p+=4){
+        for(var p = 0; p <= 12; p+=4){
             for(var i = 0; i < 4; i++){
-                boxes[p+i].style.top = num.toString()+'px';
-                boxes[p+i].style.left = (100 * i).toString()+'px';
-                boxes[p+i].style.backgroundPosition = (-100 * i).toString()+'px '+(-1 * num).toString()+'px';
+                if ((p+i) === 15) {
+                    break;
+                }
+                boxes[p+i].style.top = num+'px';
+                boxes[p+i].style.left = (100 * i)+'px';
+                boxes[p+i].style.backgroundPosition = (-100 * i)+'px '+(-1 * num)+'px';
             }
             num += 100;
         }
+    }
+
+    function selectPieces(){
+        boxes.forEach(d =>{
+            d.addEventListener('click', movePieces);
+        })
+    }
+
+    function movePieces(){
+        var tempT = this.style.top;
+        var tempL = this.style.left;
+        this.style.top = blankTile[0];
+        this.style.left = blankTile[1];
+        blankTile[0] = tempT;
+        blankTile[1] = tempL;
+    }
+
+    function shuffle(){
+        
     }
 }
